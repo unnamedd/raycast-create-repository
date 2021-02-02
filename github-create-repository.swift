2818b9d60e59612a0a9f810be7ff0246f75e7fb5
Thiago Holanda <unnamedd@gmail.com>
2021-02-02 21:13:32 +0100

    Add support to copy and paste

--- github-create-repository.swift
+++ github-create-repository.swift
@@ -53,20 +53,32 @@ extension NSApplication {
 
 extension NSApplication {
   var customMenu: NSMenu {
-    let appMenu = NSMenuItem()
-    appMenu.submenu = NSMenu()
-    
     let appName = "Create GitHub Repository"
-    appMenu.submenu?.addItem(
-      NSMenuItem(
-        title: "Quit \(appName)", 
-        action: #selector(NSApplication.terminate(_:)), 
-        keyEquivalent: "q"
-      )
-    )
     
+    let appMenu = NSMenuItem()
+    appMenu.submenu = NSMenu(title: appName)
+    appMenu.submenu?.items = [
+      NSMenuItem(title: "Quit \(appName)", action: #selector(NSApplication.terminate(_:)),  keyEquivalent: "q")
+    ]
+
+    let editMenu = NSMenuItem()
+    editMenu.submenu = NSMenu(title: "Edit")
+    editMenu.submenu?.items = [
+      NSMenuItem(title: "Cut", action: #selector(NSText.cut(_:)), keyEquivalent: "x"),
+      NSMenuItem(title: "Copy", action: #selector(NSText.copy(_:)), keyEquivalent: "c"),
+      NSMenuItem(title: "Paste", action: #selector(NSText.paste(_:)), keyEquivalent: "v"),
+      NSMenuItem.separator(),
+      NSMenuItem(title: "Select All", action: #selector(NSText.selectAll(_:)), keyEquivalent: "a"),
+    ]
+    
+    let windowMenu = NSMenuItem()
+    windowMenu.submenu = NSMenu(title: "Window")
+    windowMenu.submenu?.items = [
+        NSMenuItem(title: "Close", action: #selector(NSWindow.performClose(_:)), keyEquivalent: "w"),
+    ]
+
     let mainMenu = NSMenu(title: "Main Menu")
-    mainMenu.addItem(appMenu)
+    mainMenu.items = [appMenu, editMenu, windowMenu]
     
     return mainMenu
   }
